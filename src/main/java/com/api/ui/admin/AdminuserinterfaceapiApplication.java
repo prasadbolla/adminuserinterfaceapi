b31ce9b0f64cb27ffebdcafe1c8b970a06b2438d
Prasad Bolla <prasad.bolla1@ibm.com>
2020-01-17 00:33:37 -0500

    initial changes

---
+++ src/main/java/com/api/ui/admin/AdminuserinterfaceapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.ui.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminuserinterfaceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminuserinterfaceapiApplication.class, args);
+	}
+
+}
